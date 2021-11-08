fc059aa8a1ec89104cbea7f9336c944009fc28e5
n18020496 <nayar22@hotmail.fr>
2021-11-08 18:01:20 +0100

    Méthodes de pour la détection de collisons.

--- Moteur-Pacman/src/Moteurs/Main.java
+++ Moteur-Pacman/src/Moteurs/Main.java
@@ -2,4 +2,19 @@ package Moteurs;
 import Moteurs.physic.*;
 public class Main {
 
+
+
+
+    public static void main(String[] args) {
+        Entity mur = new Entity(2,2,2,2);
+        Entity pac = new Entity(3,3,2,2);
+
+        CollisionManage.detectCollision(mur,pac);
+
+
+    }
+
+
+
+
 }
