fc808de9f88197f84e36f8e12861a47bd88c557d
n18020496 <nayar22@hotmail.fr>
2021-11-08 18:03:32 +0100

    La classe Entity.

---
+++ Moteur-Pacman/src/Moteurs/physic/Entity.java
@@ -0,0 +1,38 @@
+package Moteurs.physic;
+
+import java.util.Vector;
+
+public class Entity {
+
+    private double posX;
+    private double posY;
+
+    private double length;
+    private double width;
+
+    public Entity(double posX, double posY,double length, double width){
+        this.posX = posX;
+        this.posY = posY;
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getPosX(){
+        return posX;
+    }
+
+    public double getPosY(){
+        return posY;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+
+
+}
