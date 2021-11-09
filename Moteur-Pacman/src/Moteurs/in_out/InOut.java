f76edd818f646b9089246ddf9aff4fa5fdcaa3c3
Eliès SAHLI <79467400+e-sahli@users.noreply.github.com>
2021-11-09 13:26:05 +0100

    Ajout commandes

--- Moteur-Pacman/src/Moteurs/in_out/InOut.java
+++
@@ -1,4 +0,0 @@
-package Moteurs.in_out;
-
-public class InOut {
-}
---
+++ Moteur-Pacman/src/Moteurs/inout/InOut.java
@@ -0,0 +1,19 @@
+package Moteurs.inout;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InOut {
+    public void keyPressed(KeyEvent evt) {
+        int code = evt.getKeyCode();
+
+        if (code == KeyEvent.VK_UP) System.out.println("Haut");
+        else if (code == KeyEvent.VK_DOWN) System.out.println("Bas");
+        else if (code == KeyEvent.VK_RIGHT) System.out.println("Droite");
+        else if (code == KeyEvent.VK_LEFT) System.out.println("Gauche");
+    }
+
+    public static void main (String args[])
+    {
+    }
+}
