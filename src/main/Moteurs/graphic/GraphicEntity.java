2be1bbb59d3f9d162fdc7dae43337df024d2f293
BatBapt <lerouxbaptistepro2@gmail.com>
2021-11-17 10:36:24 +0100

    getter & setter

--- src/main/Moteurs/graphic/GraphicEntity.java
+++ src/main/Moteurs/graphic/GraphicEntity.java
@@ -1,12 +1,69 @@
 package Moteurs.graphic;
 
+import java.awt.*;
+
 public class GraphicEntity {
 
     private int x; // Position x
     private int y; // Position y
-
     private int height;
     private int width;
 
+    private Color color;
+
+
     protected Scene scene;
+
+    // Getters
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    // Setters
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
 }
