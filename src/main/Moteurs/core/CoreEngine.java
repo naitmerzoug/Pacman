53dfba4f1ca9f6c7d17ff3c4bcc408fe39f75dc1
Eliès SAHLI <79467400+e-sahli@users.noreply.github.com>
2021-11-29 18:09:39 +0100

    Ajout de méthodes pour le moteur noyau

--- src/main/Moteurs/core/CoreEngine.java
+++ src/main/Moteurs/core/CoreEngine.java
@@ -1,8 +1,12 @@
 package Moteurs.core;
 
-import Moteurs.graphic.GraphicEngine;
+import Moteurs.graphicm.GraphicEngine;
+import Moteurs.graphicm.GraphicEntity;
 import Moteurs.inout.IOEngine;
 import Moteurs.physic.PhysicEngine;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Noyau du jeu
@@ -13,11 +17,31 @@ public class CoreEngine {
     private IOEngine ioEngine;
     private GraphicEngine graphicEngine;
     private PhysicEngine physicEngine;
+    private ConcurrentMap<Integer,CoreEntity> entities;
+    public static int nbEntities = 0;
 
     public CoreEngine(){
         this.ioEngine = new IOEngine();
         this.graphicEngine = new GraphicEngine();
         this.physicEngine = new PhysicEngine();
+        this.entities = new ConcurrentHashMap<>();
+    }
+
+    public static void main(String args[]){
+        System.out.println("test");
+    }
+
+    public void addEntity(CoreEntity e){
+        if(e.getId()==0 || entities.containsKey(e.getId())){
+            nbEntities++;
+            e.setId(nbEntities);
+        }
+        entities.put(e.getId(),e);
+        this.getPhysicEngine().createEntity(e.getPhysicEntity());
+    }
+
+    public void removeEntity(CoreEntity e){
+        entities.remove(e.getId());
     }
 
     // Getter //
@@ -33,4 +57,19 @@ public class CoreEngine {
     public PhysicEngine getPhysicEngine() {
         return physicEngine;
     }
+
+    public static int getNbEntities() {
+        return nbEntities;
+    }
+
+    public ConcurrentMap<Integer, CoreEntity> getEntities() {
+        return entities;
+    }
+
+    // Setter
+
+    public static void setNbEntities(int nbEntities) {
+        CoreEngine.nbEntities = nbEntities;
+    }
+
 }
