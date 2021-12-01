cd721eb942b255b8a544ab24b0f9ec0d2b3b2c59
Rayane NAIT MERZOUG <56247297+naitmerzoug@users.noreply.github.com>
2021-12-01 15:43:02 +0100

    Création des quatre ghost car les quatres ont un algorithme de deplacement unique.

--- src/main/PacmanGame/OrangeGhost.java
+++ src/main/PacmanGame/OrangeGhost.java
@@ -3,7 +3,13 @@ package PacmanGame;
 import Moteurs.core.CoreEngine;
 
 public class OrangeGhost extends Ghost{
-    public OrangeGhost(int x, int y, CoreEngine coreEngine) {
-        super(x, y, coreEngine);
+
+
+    private double initialX;
+    private double initialY;
+
+    public OrangeGhost(int initialX, int initialY, CoreEngine coreEngine) {
+        super(initialX, initialY, coreEngine);
     }
+
 }
