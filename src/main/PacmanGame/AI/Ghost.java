0753e56d4cc45c8587681fd84f58dc3c46a6b5c9
hamoydy <hamoydydia@gmail.com>
2021-12-02 12:01:55 +0100

    Ajout de l'IA dans la GamePlay Ajout de STOP dans l'enum Direction Modif de Ghost

--- src/main/PacmanGame/AI/Ghost.java
+++ src/main/PacmanGame/AI/Ghost.java
@@ -1,74 +1,146 @@
 package PacmanGame.AI;
 
 import Moteurs.core.CoreEngine;
+import PacmanGame.Direction;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public abstract class Ghost {
-
-
-    public static final double basicSpeed = 1;
-    public static final double scaredSpeed = 1.5;
-    public static final double speed= 1.7;
-
-    protected double initialX;
-    protected double initialY;
-    private boolean isVulnerable;
-    private CoreEngine coreEngine;
-
-    public enum State{
-        TRACK,
-        SCARED,
-        DEAD,
-        NULL
+    private int speed = 1;
+    // color = 1 Blinky, Color = 2 PINK, Color = 3 Clyde.
+    public Direction nextDirection;
+    private int color;
+    public AiInterface AI;
+    public int getSpeed() {
+        return speed;
     }
-    protected State state = State.NULL;
-
-    public Ghost(int initialX, int initialY, CoreEngine coreEngine)
-    {
-        this.initialX = initialX;
-        this.initialY = initialY;
-        this.isVulnerable = false;
-        this.coreEngine = coreEngine;
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public int getColor() {
+        return color;
+    }
+    public void setColor(int color) {
+        this.color = color;
     }
 
+    public Ghost(int x, int y, int color){
+        this.setPosition(new Point(x, y));
+        this.setPixelPosition(new Point(10+getSize()*x, 10+getSize()*y));
+        this.color = color;
+        switch (color){
+            case 1:{
+                Image image = new ImageIcon(this.getURLPath("Image/Ghost/Blinky/Blinky-0.png")).getImage();
+                this.setImage(image);
+                break;
+            }
+            case 2: {
+                Image image = new ImageIcon(this.getURLPath("Image/Ghost/Pinky/Pinky-0.png")).getImage();
+                this.setImage(image);
+                break;
+            }
+            case 3: {
+                Image image = new ImageIcon(this.getURLPath("Image/Ghost/Clyde/Clyde-0.png")).getImage();
+                this.setImage(image);
+                break;
+            }
+            default: {
+                Image image = new ImageIcon(this.getURLPath("Image/Ghost/Inky/Inky-0.png")).getImage();
+                this.setImage(image);
+                break;
+            }
+        }
 
-
-    //Getters
-
-
-    public double getInitialX() {
-        return initialX;
     }
 
-    public double getInitialY() {
-        return initialY;
+    public Ghost(Point position, GameBoard board){
+        this.setDirection(Direction.STOP);
+        this.setPosition(position);
+        this.setPixelPosition(new Point(position.x*28, position.y*28));
+        this.setGameBoard(board);
+        try {
+            String path = System.getProperty("user.dir");
+            Image image = ImageIO.read(new File(path+ "/resources/Ghost/7.png"));
+            this.setImage(image);
+            //this.image = ImageIO.read(this.getClass().getResource("resources/Ghost/7.png"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
-    public boolean isVulnerable() {
-        return isVulnerable;
+    public void nextMoveCalculateByAI(CoreEngine coreEngine){
+        Direction direction = this.AI.getMovement(this, coreEngine);
+        //System.out.println("movement: " + direction);
+        this.setDirection(direction);
     }
 
-    public CoreEngine getCoreEngine() {
-        return coreEngine;
+    @Override
+    public Boolean canMove() {
+        return true;
     }
 
-    // Setters
-
-
-    public void setInitialX(double initialX) {
-        this.initialX = initialX;
+    @Override
+    public void move() {
+        System.out.println("in ghost move: " + this.getDirection());
+        switch (this.getDirection()) {
+            case UP:
+                this.getPixelPosition().y -= speed;
+                this.getGameBoard().getGraphics().drawImage(this.getImage(), getPixelPosition().x, getPixelPosition().y, null);
+                this.getGameBoard().repaint();
+                break;
+            case DOWN:
+                this.getPixelPosition().y += speed;
+                this.getGameBoard().getGraphics().drawImage(this.getImage(), getPixelPosition().x, getPixelPosition().y, null);
+                this.getGameBoard().repaint();
+                break;
+            case LEFT:
+                this.getPixelPosition().x -= speed;
+                this.getGameBoard().getGraphics().drawImage(this.getImage(), getPixelPosition().x, getPixelPosition().y, null);
+                this.getGameBoard().repaint();
+                break;
+            case RIGHT:
+                this.getPixelPosition().x += speed;
+                this.getGameBoard().getGraphics().drawImage(this.getImage(), getPixelPosition().x, getPixelPosition().y, null);
+                this.getGameBoard().repaint();
+                break;
+            default: break;
+        }
     }
 
-    public void setInitialY(double initialY) {
-        this.initialY = initialY;
+    @Override
+    public String name() {
+        return null;
     }
 
-    public void setVulnerable(boolean vulnerable) {
-        this.isVulnerable = vulnerable;
-         //if vulnerable true, va plus lentement
+    @Override
+    public void goUp() {
+        this.nextDirection = Direction.UP;
     }
 
-    public void setCoreEngine(CoreEngine coreEngine) {
-        this.coreEngine = coreEngine;
+    @Override
+    public void goDown() {
+        this.nextDirection = Direction.DOWN;
     }
 
+    @Override
+    public void goLeft() {
+        this.nextDirection = Direction.LEFT;
+    }
+
+    @Override
+    public void goRight() {
+        this.nextDirection = Direction.RIGHT;
+    }
+
+    @Override
+    public void stop() {
+        this.nextDirection = Direction.STOP;
+    }
 }
+
