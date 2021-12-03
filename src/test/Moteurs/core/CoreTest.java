258feb83424ba3737010c3081cdf8b9342d8d86d
Eliès SAHLI <79467400+e-sahli@users.noreply.github.com>
2021-12-03 14:08:09 +0100

    rectifications NOYAU

--- src/test/Moteurs/core/CoreTest.java
+++ src/test/Moteurs/core/CoreTest.java
@@ -40,11 +40,10 @@ public class CoreTest {
         Moteurs.core.CoreEngine coreEngine = new Moteurs.core.CoreEngine("Game",20,20);
         CoreEntity e1 = coreEngine.createAndAddEntity(Type.SOFT, 1,1,10,10, 1, new File("src/main/Pacman/Images/pacman.png"));
         coreEngine.removeEntity(e1);
-        System.out.println(coreEngine.getGraphicEngine().getFrame().getComponents());
         Assertions.assertAll(
                 () ->  assertTrue(coreEngine.getEntities().isEmpty()),
                 () ->  assertTrue(coreEngine.getPhysicEngine().getEntities().isEmpty()),
-                () ->  assertEquals(coreEngine.getGraphicEngine().getFrame().getComponentCount(), 0)
+                () ->  assertEquals(coreEngine.getGraphicEngine().getFrame().getComponentCount(), 1)
         );
 
     }
